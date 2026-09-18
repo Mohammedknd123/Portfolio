d4f0062905d911fee7f108c2ba911f1c5d68c4c6
Mohammedknd123 <mohamedknd123@gmail.com>
2026-09-18 14:34:52 +0100

    Set default theme mode to dark

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 
 export default function ThemeToggle({ className }) {
-  const [isDarkMode, setDarkMode] = useState(false);
+  const [isDarkMode, setDarkMode] = useState(true);
 
   const toggleTheme = () => {
     if (isDarkMode) {
